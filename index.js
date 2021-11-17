ee155e110b4d92be4eeb4ec3407841298b38dc5d
klukvas <programmer56457@gmail.com>
2021-11-17 23:34:47 +0200

    added first test | set token from first call as global

--- index.js
+++ index.js
@@ -15,80 +15,84 @@ class Connector{
             email: this.email,
             password: this.password
         };
-        const res = await axios.post(this.auth_url, user_data);
         try{
-            const token = res.data.data.token;
-            return token
-        }catch (error){
-            return res.status
+            const res = await axios.post(this.auth_url, user_data);
+            try{
+                const token = res.data.data.token;
+                return token
+            }catch (error){
+                return res.status
+            }
+        }catch (err){
+            return {'Err_sending_request': err}
         }
+        
     };
 
     create_connection(){
         const connection = new signalR.HubConnectionBuilder()
             .withUrl(this.server_url)
+            .configureLogging(signalR.LogLevel.Error)
             .build();
         return connection;
     };
-    async config_connection(){
-        let counter = 0;
+    async config_connection(token){
         const connection = this.create_connection();
-        const token = await this.get_auth_token();
         connection.on("welcome", (res) => {
             this.messages['welcome'] = res
             connection.off('welcome')
-            counter++
+            
         });
         connection.on("asset-list", (res) => {
             this.messages['asset-list'] = res
             connection.off('asset-list')
-            counter++
+            
 
         });
         connection.on("spot-insrument-list", (res) => {
             this.messages['spot-insrument-list'] = res
             connection.off('spot-insrument-list')
-            counter++
+            
 
 
         });
         connection.on("spot-wallet-balances", (res) => {
             this.messages['spot-wallet-balances'] = res
             connection.off('spot-wallet-balances')
-            counter++
+            
 
         });
         connection.on("spot-active-orders", (res) => {
             this.messages['spot-active-orders'] = res
             connection.off('spot-active-orders')
-            counter++
+            
 
         });
         connection.on("market-reference", (res) => {
             this.messages['market-reference'] = res
             connection.off('market-reference')
-            counter++
+            
 
 
         });
         connection.on("key-values", (res) => {
             this.messages['key-values'] = res
             connection.off('key-values')
-            counter++
+            
 
 
         });
         connection.on("base-prices", (res) => {
             this.messages['base-prices'] = res
             connection.off('base-prices')
-            counter++
+            
 
 
         });
         connection.on("prices-base-currency", (res) => {
             this.messages['prices-base-currency'] = res
             connection.off('prices-base-currency')
-            counter++
+            
 
 
         });
@@ -99,29 +103,14 @@ class Connector{
             let counter = 0;
             let intervalId = setInterval(() => {
                 counter++
-                console.log(`Counter1: ${counter}`)
-                try{
-                    if(Object.keys(this.messages).length == 9) {
-                        resolve({'status': 1, 'data': this.messages, 'intervalId': intervalId, 'connection': connection});
-                    }else if(counter > 6){
-                        console.log(`Counter: ${counter}`)
-                        resolve({'status': 0, 'data': this.messages, 'intervalId': intervalId, 'connection': connection})
-                    }
-                }catch(e1){
-                    console.log(`e1: ${e1}`)
+                if(Object.keys(this.messages).length == 8) {
+                    resolve({'status': 1, 'data': this.messages, 'intervalId': intervalId, 'connection': connection});
+                }else if(counter > 6){
+                    resolve({'status': 0, 'data': this.messages, 'intervalId': intervalId, 'connection': connection})
                 }
-              
             }, 1000);
           });
-    }
-
-    get_messages(){
-        return this.messages
     }
 }
 
 module.exports = Connector;
-let test = new Connector();
-test.config_connection().then(
-    mess=> {console.log(mess.status); clearInterval(mess.intervalId), mess.connection.stop()},
-).catch(error => { throw error})
--- test.js
+++ test.js
@@ -3,37 +3,72 @@ var Connector = require('./index')
 var expect = require('chai').expect
 
 
-
-
-
-describe('Tests of messages from signalR hubs', () => {
-    let token;
-    let signalClass;
-    let conn;
+describe('Tests of auth(login)', () => {
     before(async () => {
         signalClass = new Connector();
-        token = await signalClass.get_auth_token();
-        conn = await signalClass.create_connection();
-        signalClass.config_connection();
-        let counter;
-        let id = setInterval(() => {
-            counter++;
-            console.log(`Keys in interval: ${Object.keys(signalClass.get_messages())}`)
-            let messages = signalClass.get_messages();
-            if(Object.keys(messages) == 9){
-                clearInterval(id);
-            }
-            if(counter > 6){
-                //pass
-            }
-        }, 2000)
+        global.token = await signalClass.get_auth_token();
     });
 
-    afterEach(async () => {
-        await conn.stop();
+    it('Check that token are correct', (done) => {
+        expect(global.token).to.be.an('string');
+        done()
+    });
+})
+
+describe('Tests of messages from signalR hubs', () => {
+    let signalClass;
+    let messagesObject;
+    before(async () => {
+        signalClass = new Connector();
+        messagesObject = await signalClass.config_connection(global.token);
+        clearInterval(messagesObject.intervalId);
+        messagesObject.connection.stop()
     });
 
     it('Tests of message from asset-list hub', (done) => {
-            console.log(Object.keys(signalClass.get_messages()))
+        expect( Object.keys( messagesObject.data ) ).to.include('asset-list');
+        expect(messagesObject.data['asset-list']).to.be.an('object').that.has.all.keys(
+            'assets', 'now'
+        );
+        for(let item of messagesObject.data['asset-list']['assets']){
+            expect(item).to.be.an('object').that.has.all.keys(
+                "symbol",
+                "description",
+                "prefixSymbol",
+                "accuracy",
+                "depositMode",
+                "withdrawalMode",
+                "tagType",
+                "iconUrl",
+                "fees",
+                "assetType",
+                "isMainNet",
+                "isEnabled",
+                "depositMethods",
+                "withdrawalMethods",
+                "depositBlockchains",
+                "withdrawalBlockchains"
+            );
+            expect(item.symbol).to.be.an('string');
+            expect(item.description).to.be.an('string');
+            expect(item.prefixSymbol).to.be.oneOf(["$", "€", null]);
+            expect(item.accuracy).to.be.an('number');
+            expect(item.depositMode).to.be.an('number');
+            expect(item.withdrawalMode).to.be.an('number');
+            expect(item.tagType).to.be.an('number');
+            expect(item.iconUrl).to.be.an('string');
+            expect(item.fees).to.be.an('object').that.has.all.keys(
+                "withdrawalFee"
+            );
+            expect(typeof item.fees.withdrawalFee).to.be.oneOf(['object', "number"]);
+            expect(item.assetType).to.be.an('string');
+            expect(item.isMainNet).to.be.an('boolean');
+            expect(item.isEnabled).to.be.an('boolean');
+            expect(item.depositMethods).to.be.an('array');
+            expect(item.withdrawalMethods).to.be.an('array');
+            expect(item.depositBlockchains).to.be.an('array');
+            expect(item.withdrawalBlockchains).to.be.an('array');
+        }
+        done();
     });
 });
