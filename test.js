8e86e29be27e69c925e583fcaf0ede07552a1d64
klukvas <programmer56457@gmail.com>
2021-11-18 11:30:02 +0200

    added tests for all hubs

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const signalR = require("@microsoft/signalr");
 var Connector = require('./index')
 var expect = require('chai').expect
+require('dotenv').config();
 
 
 describe('Tests of auth(login)', () => {
@@ -19,6 +20,8 @@ describe('Tests of messages from signalR hubs', () => {
     let signalClass;
     let messagesObject;
     before(async () => {
+        clientId = process.env.clientId
+        clientWalletId = process.env.clientWalletId
         signalClass = new Connector();
         messagesObject = await signalClass.config_connection(global.token);
         clearInterval(messagesObject.intervalId);
@@ -71,4 +74,184 @@ describe('Tests of messages from signalR hubs', () => {
         }
         done();
     });
+
+    it('Check "welcome" Hub ', (done) => {
+        let res = messagesObject.data['welcome']
+        expect(res.data).to.be.eql(`Hello ${clientId}/${clientWalletId}`)
+        expect(res).to.be.an('object').that.has.all.keys(
+            'data', 'now'
+        );
+        done();
+
+    });
+
+    it('Check "spot-insrument-list" Hub ', (done) => {
+        let res = messagesObject.data['spot-insrument-list']
+        expect(res).to.be.an('object').that.has.all.keys(
+            'spotInstruments', 'now'
+        );
+        expect(res.spotInstruments).to.be.an('array');
+        for(let item of res.spotInstruments){
+            expect(item).to.be.an('object').that.has.all.keys(
+                'symbol',
+                'baseAsset',
+                'quoteAsset',
+                'accuracy',
+                'minVolume',
+                'maxVolume',
+                'maxOppositeVolume',
+                'iconUrl'
+            );
+            expect(item.symbol).to.be.an('string');
+            expect(item.baseAsset).to.be.an('string');
+            expect(item.quoteAsset).to.be.an('string');
+            expect(item.accuracy).to.be.an('number');
+            expect(item.minVolume).to.be.an('number');
+            expect(item.maxVolume).to.be.an('number');
+            expect(item.maxOppositeVolume).to.be.an('number');
+            expect(typeof item.iconUrl).to.be.oneOf(['object', "number"]);
+        }
+        done();
+
+    });
+
+    it('Check "spot-wallet-balances" Hub ', (done) => {
+        let res = messagesObject.data['spot-wallet-balances']
+        expect(res).to.be.an('object').that.has.all.keys(
+            'balances'
+        );
+        expect(res.balances).to.be.an('array');
+        for(let item of res.balances){
+            expect(item).to.be.an('object').that.has.all.keys(
+                'avgBuyingPrice',
+                'apy',
+                'apr',
+                'accumulationAmount',
+                'assetId',
+                'balance',
+                'reserve',
+                'lastUpdate',
+                'sequenceId'
+            );
+            expect(item.avgBuyingPrice).to.be.an('number');
+            expect(item.apy).to.be.an('number');
+            expect(item.accumulationAmount).to.be.an('number');
+            expect(item.assetId).to.be.an('string');
+            expect(item.balance).to.be.an('number');
+            expect(item.reserve).to.be.an('number');
+            expect(item.lastUpdate).to.be.an('string');
+            expect(item.sequenceId).to.be.an('number');
+        }
+        done();
+
+    });
+
+    it('Check "market-reference" Hub ', (done) => {
+        let res = messagesObject.data['market-reference']
+        expect(res).to.be.an('object').that.has.all.keys(
+            'references', 'now'
+        );
+        expect(res.references).to.be.an('array');
+        for(let item of res.references){
+            expect(item).to.be.an('object').that.has.all.keys(
+                'id',
+                'brokerId',
+                'name',
+                'iconUrl',
+                'associateAsset',
+                'associateAssetPair',
+                'weight',
+                'isMainNet'
+            );
+            expect(item.id).to.be.an('string');
+            expect(item.brokerId).to.be.an('string');
+            expect(item.iconUrl).to.be.an('string');
+            expect(item.associateAsset).to.be.an('string');
+            expect(item.associateAssetPair).to.be.an('string');
+            expect(item.weight).to.be.an('number');
+            expect(item.isMainNet).to.be.an('boolean');
+        }
+        done();
+
+    });
+
+    it('Check "prices-base-currency" Hub ', (done) => {
+        let res = messagesObject.data['prices-base-currency']
+        expect(res).to.be.an('object').that.has.all.keys(
+            'P', 'now'
+        );
+        expect(res.P).to.be.an('array');
+        for(let item of res.P){
+            expect(item).to.be.an('object').that.has.all.keys(
+                'S',
+                'T',
+                'P',
+                'P24p',
+                'P24a'
+            );
+            expect(item.S).to.be.an('string');
+            expect(item.T).to.be.an('number');
+            expect(item.P).to.be.an('number');
+            expect(item.P24p).to.be.an('number');
+            expect(item.P24a).to.be.an('number');
+        }
+        done();
+
+    });
+
+    it('Check "base-prices" Hub ', (done) => {
+        let res = messagesObject.data['base-prices']
+
+        expect(res).to.be.an('object').that.has.all.keys(
+            'priceRecords', 'now'
+        );
+        expect(res.priceRecords).to.be.an('array');
+        for(let item of res.priceRecords){
+            expect(item).to.be.an('object').that.has.all.keys(
+                'brokerId',
+                'assetSymbol',
+                'currentPrice',
+                'h24P',
+                'h24',
+                'd7',
+                'm1',
+                'm3'
+            );
+            expect(item.brokerId).to.be.eql('jetwallet');
+            expect(item.assetSymbol).to.be.an('string');
+            expect(item.currentPrice).to.be.an('number');
+            
+            expect(item.h24P).to.be.an('number');
+
+            expect(item.h24).to.be.an('object').that.has.all.keys(
+                'price', 'recordTime'
+            );
+            expect(item.h24.price).to.be.an('number');
+            expect(item.h24.recordTime).to.be.an('string');
+            
+            
+            expect(item.d7).to.be.an('object').that.has.all.keys(
+                'price', 'recordTime'
+            );
+            expect(item.d7.price).to.be.an('number');
+            expect(item.d7.recordTime).to.be.an('string');
+
+            
+            expect(item.m1).to.be.an('object').that.has.all.keys(
+                'price', 'recordTime'
+            );
+            expect(item.m1.price).to.be.an('number');
+            expect(item.m1.recordTime).to.be.an('string');
+
+            
+            expect(item.m3).to.be.an('object').that.has.all.keys(
+                'price', 'recordTime'
+            );
+            expect(item.m3.price).to.be.an('number');
+            expect(item.m3.recordTime).to.be.an('string');
+
+        }
+        done();
+    });
+
 });
